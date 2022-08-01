5fa24a1ef0eaf58cc4fabee1008c079b3fa7ce59
Ross <rosssullivan101@gmail.com>
2022-08-01 10:14:48 +0900

    Added Github link

--- src/components/LinksSection.tsx
+++ src/components/LinksSection.tsx
@@ -1,10 +1,21 @@
+import Image from "next/image";
+import githubLogo from "../../assets/logos/github-logo.png";
 
+const githubLink = 'https://github.com/ranger-ross';
 
 const LinksSection = () => {
+    const logoSize = 35;
     return (
-        <div className="flex gap-2 m-2 justify-center">
-            <div>Github link placeholder</div>
-            <div>ect</div>
+        <div className="flex gap-2 justify-center">
+            <div className="duration-500 motion-safe:hover:scale-110 cursor-pointer">
+                <a href={githubLink} target={"_blank"} rel="noreferrer">
+                    <Image src={githubLogo}
+                           height={logoSize}
+                           width={logoSize}
+                           alt={"github"}
+                    />
+                </a>
+            </div>
         </div>
     );
 };
